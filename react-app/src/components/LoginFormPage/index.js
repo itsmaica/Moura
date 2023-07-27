c62fbac5cfa2f6176cac878b493f463b7ce5a225
Maica <maicasantos90@gmail.com>
2023-07-26 17:53:46 -0700

    starting demo user feature

--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { login } from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-import './LoginForm.css';
+import "./LoginForm.css";
 
 function LoginFormPage() {
   const dispatch = useDispatch();
@@ -23,49 +23,51 @@ function LoginFormPage() {
 
   return (
     <>
-      <div className='div-background-img' id='loginform-container'>
-
-
-        <form id='login-form' onSubmit={handleSubmit}>
-
-          <div id='lf-header-and-tagline'>
-            <h1 className='tile' id='lf-moura'>Moura</h1>
-            <h3 id='lf-tagline'>A place to share knowledge and better understand the world of house plants</h3>
+      <div className="div-background-img" id="loginform-container">
+        <form id="login-form" onSubmit={handleSubmit}>
+          <div id="lf-header-and-tagline">
+            <h1 className="tile" id="lf-moura">
+              Moura
+            </h1>
+            <h3 id="lf-tagline">
+              A place to share knowledge and better understand the world of
+              house plants
+            </h3>
           </div>
 
-          <div id='lf-input-container'>
+          <div id="lf-input-container">
             <p>Login</p>
 
-            <div id='lf-divider-line'></div>
+            <div id="lf-divider-line"></div>
 
             <ul>
               {errors.map((error, idx) => (
                 <li key={idx}>{error}</li>
               ))}
             </ul>
-            <label>
-              Email
-            </label>
-              <input
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            <label>
-              Password
-            </label>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+            <label>Email</label>
+            <input
+              placeholder="Your email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label>Password</label>
+            <input
+              placeholder="Your password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
 
-              <div id='lf-button-cont'>
+            <div id="lf-button-cont">
+              <button id='demo-button'>Demo User</button>
+              <button type="submit">Login</button>
 
-                <button type="submit">Login</button>
-              </div>
+
+            </div>
           </div>
 
         </form>
